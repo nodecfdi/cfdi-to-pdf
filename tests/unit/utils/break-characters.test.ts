edaf25f6d683796357e78e55483a7a3ccd18251b
Fernando Isidro L <luffynando@gmail.com>
2022-07-04 15:33:04 -0500

    test: increment coverage to 100% on utils

--- tests/unit/utils/break-characters.test.ts
+++ tests/unit/utils/break-characters.test.ts
@@ -13,4 +13,33 @@ describe('Name of the group', () => {
             expect(line.length).toBe(86);
         });
     });
+
+    test('brakes default', () => {
+        const str =
+            'hsHwe8CjQpNClpK6d8FqyONW1OsDjMATXYN29ldPz1KseRy6E0SRcI42VKbg184xTaZhDrhFdgr1a5VH1ufaFZBr3m3fPOWUOG+feb3iMZZZH2UQoELqUn3c+6ZVFgAW9ksttIeKvxbGWsFIAB8A/b+dC58mPvIfOFVksmCIqaQ8';
+
+        const result2 = breakEveryNCharacters(str);
+
+        const arrayResult = result2.split('\n');
+        arrayResult.forEach((line) => {
+            expect(line.length).toBe(86);
+        });
+    });
+
+    test('brakes is minor or equal to specific characters break', () => {
+        const n = 100;
+        const str =
+            'hsHwe8CjQpNClpK6d8FqyONW1OsDjMATXYN29ldPz1KseRy6E0SRcI42VKbg184xTaZhDrhFdgr1a5VH1ufaFZBr3m3fPOWUOG+';
+
+        const result2 = breakEveryNCharacters(str, n);
+
+        const arrayResult = result2.split('\n');
+        arrayResult.forEach((line) => {
+            expect(line.length).toBeLessThanOrEqual(n);
+        });
+    });
+
+    test('return empty on empty input', () => {
+        expect(breakEveryNCharacters('')).toBe('');
+    });
 });
