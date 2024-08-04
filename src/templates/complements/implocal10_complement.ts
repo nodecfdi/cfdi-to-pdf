639aceef3e75795cf6d59334c253bf835294482f
Fernando Isidro Luna <luffynando@gmail.com>
2024-08-04 17:02:03 -0600

    refactor: :recycle: move implocal 10 to complements

---
+++ src/templates/complements/implocal10_complement.ts
@@ -0,0 +1,168 @@
+import { type XmlNodeInterface } from '@nodecfdi/cfdi-core/types';
+import { type TableCell } from 'pdfmake/interfaces.js';
+import { formatCurrency, toNumber } from '#src/utils/currency';
+
+const useImplocal10Complement = (
+  impLocal10: XmlNodeInterface,
+  primaryColor: string,
+  bgGrayColor: string,
+  tableTotales: TableCell[][],
+  totalesContent: TableCell[],
+): void => {
+  const totalesSubContent: TableCell[] = [];
+  const totalRetencionesLocales = impLocal10.getAttribute('TotaldeRetenciones');
+  const totalTrasladosLocales = impLocal10.getAttribute('TotaldeTraslados');
+
+  if (toNumber(totalTrasladosLocales) > 0) {
+    tableTotales.push([
+      { text: 'Traslados Locales', alignment: 'right' },
+      { text: '$', color: primaryColor, alignment: 'center' },
+      { text: formatCurrency(totalTrasladosLocales, 'code'), alignment: 'right' },
+    ]);
+  }
+
+  if (toNumber(totalRetencionesLocales) > 0) {
+    tableTotales.push([
+      { text: 'Retenciones Locales', alignment: 'right' },
+      { text: '$', color: primaryColor, alignment: 'center' },
+      { text: `- ${formatCurrency(totalRetencionesLocales, 'code')}`, alignment: 'right' },
+    ]);
+  }
+
+  const trasladosLocales = impLocal10.searchNodes('implocal:TrasladosLocales');
+  const retencionesLocales = impLocal10.searchNodes('implocal:RetencionesLocales');
+
+  const retencionesLocalesTable: TableCell = {
+    table: {
+      widths: ['40%', '20%', 'auto'],
+      body: [
+        [
+          {
+            text: 'Impuestos Locales Retenidos',
+            style: ['tableSubtitleHeader'],
+            color: primaryColor,
+            colSpan: 3,
+          },
+          '',
+          '',
+        ],
+        ...retencionesLocales.map((retencionLocal) => {
+          return [
+            {
+              text: retencionLocal.getAttribute('ImpLocRetenido'),
+              fillColor: bgGrayColor,
+            },
+            {
+              text: [
+                {
+                  text: 'Tasa: ',
+                },
+                {
+                  text: `${(Number(retencionLocal.getAttribute('TasadeRetencion')) * 1).toString()} %`,
+                },
+              ],
+              fillColor: bgGrayColor,
+            },
+            {
+              text: [
+                { text: 'Importe: ' },
+                { text: formatCurrency(retencionLocal.getAttribute('Importe')) },
+              ],
+              alignment: 'right',
+              fillColor: bgGrayColor,
+            },
+          ];
+        }),
+      ],
+      dontBreakRows: true,
+    },
+    layout: 'tableLayout',
+  };
+  const trasladosLocalesTable: TableCell = {
+    table: {
+      widths: ['40%', '20%', 'auto'],
+      body: [
+        [
+          {
+            text: 'Impuestos Locales Trasladados',
+            style: ['tableSubtitleHeader'],
+            color: primaryColor,
+            colSpan: 3,
+          },
+          '',
+          '',
+        ],
+        ...trasladosLocales.map((trasladoLocal) => {
+          return [
+            {
+              text: trasladoLocal.getAttribute('ImpLocTrasladado'),
+              fillColor: bgGrayColor,
+            },
+            {
+              text: [
+                {
+                  text: 'Tasa: ',
+                },
+                {
+                  text: `${(Number(trasladoLocal.getAttribute('TasadeTraslado')) * 1).toString()} %`,
+                },
+              ],
+              fillColor: bgGrayColor,
+            },
+            {
+              text: [
+                { text: 'Importe: ' },
+                { text: formatCurrency(trasladoLocal.getAttribute('Importe')) },
+              ],
+              alignment: 'right',
+              fillColor: bgGrayColor,
+            },
+          ];
+        }),
+      ],
+      dontBreakRows: true,
+    },
+    layout: 'tableLayout',
+  };
+
+  if (trasladosLocales.length === 0 && retencionesLocales.length === 0) {
+    totalesSubContent.push('', '');
+  }
+
+  if (trasladosLocales.length === 0 && retencionesLocales.length > 0) {
+    totalesSubContent.push(
+      {
+        ...retencionesLocalesTable,
+        colSpan: 2,
+      },
+      '',
+    );
+  }
+
+  if (trasladosLocales.length > 0 && retencionesLocales.length === 0) {
+    totalesSubContent.push(
+      {
+        ...trasladosLocalesTable,
+        colSpan: 2,
+      },
+      '',
+    );
+  }
+
+  if (trasladosLocales.length > 0 && retencionesLocales.length > 0) {
+    totalesSubContent.push(trasladosLocalesTable, retencionesLocalesTable);
+  }
+
+  totalesSubContent.push({
+    table: {
+      widths: ['*', '10%', 'auto'],
+      body: tableTotales,
+      dontBreakRows: true,
+    },
+    layout: 'tableLayout',
+  });
+
+  totalesContent.push(totalesSubContent);
+};
+
+export default useImplocal10Complement;
--- src/templates/sections/generic_cfdi_totales_content.ts
+++ src/templates/sections/generic_cfdi_totales_content.ts
@@ -1,6 +1,7 @@
 import { type XmlNodeInterface } from '@nodecfdi/cfdi-core/types';
 import { type Content, type TableCell } from 'pdfmake/interfaces.js';
 import { getValueOfCatalog } from '#src/catalogs/catalogs_source';
+import useImplocal10Complement from '#src/templates/complements/implocal10_complement';
 import { type CatalogsData } from '#src/types';
 import { formatCurrency, toNumber } from '#src/utils/currency';
 
@@ -145,169 +146,6 @@ const fillCfdiImpuestos = (
   }
 };
 
-const fillComplementoImpLocal10 = (
-  impLocal10: XmlNodeInterface,
-  primaryColor: string,
-  bgGrayColor: string,
-  tableTotales: TableCell[][],
-  totalesContent: TableCell[],
-): void => {
-  const totalesSubContent: TableCell[] = [];
-  const totalRetencionesLocales = impLocal10.getAttribute('TotaldeRetenciones');
-  const totalTrasladosLocales = impLocal10.getAttribute('TotaldeTraslados');
-
-  if (toNumber(totalTrasladosLocales) > 0) {
-    tableTotales.push([
-      { text: 'Traslados Locales', alignment: 'right' },
-      { text: '$', color: primaryColor, alignment: 'center' },
-      { text: formatCurrency(totalTrasladosLocales, 'code'), alignment: 'right' },
-    ]);
-  }
-
-  if (toNumber(totalRetencionesLocales) > 0) {
-    tableTotales.push([
-      { text: 'Retenciones Locales', alignment: 'right' },
-      { text: '$', color: primaryColor, alignment: 'center' },
-      { text: `- ${formatCurrency(totalRetencionesLocales, 'code')}`, alignment: 'right' },
-    ]);
-  }
-
-  const trasladosLocales = impLocal10.searchNodes('implocal:TrasladosLocales');
-  const retencionesLocales = impLocal10.searchNodes('implocal:RetencionesLocales');
-
-  const retencionesLocalesTable: TableCell = {
-    table: {
-      widths: ['40%', '20%', 'auto'],
-      body: [
-        [
-          {
-            text: 'Impuestos Locales Retenidos',
-            style: ['tableSubtitleHeader'],
-            color: primaryColor,
-            colSpan: 3,
-          },
-          '',
-          '',
-        ],
-        ...retencionesLocales.map((retencionLocal) => {
-          return [
-            {
-              text: retencionLocal.getAttribute('ImpLocRetenido'),
-              fillColor: bgGrayColor,
-            },
-            {
-              text: [
-                {
-                  text: 'Tasa: ',
-                },
-                {
-                  text: `${(Number(retencionLocal.getAttribute('TasadeRetencion')) * 1).toString()} %`,
-                },
-              ],
-              fillColor: bgGrayColor,
-            },
-            {
-              text: [
-                { text: 'Importe: ' },
-                { text: formatCurrency(retencionLocal.getAttribute('Importe')) },
-              ],
-              alignment: 'right',
-              fillColor: bgGrayColor,
-            },
-          ];
-        }),
-      ],
-      dontBreakRows: true,
-    },
-    layout: 'tableLayout',
-  };
-  const trasladosLocalesTable: TableCell = {
-    table: {
-      widths: ['40%', '20%', 'auto'],
-      body: [
-        [
-          {
-            text: 'Impuestos Locales Trasladados',
-            style: ['tableSubtitleHeader'],
-            color: primaryColor,
-            colSpan: 3,
-          },
-          '',
-          '',
-        ],
-        ...trasladosLocales.map((trasladoLocal) => {
-          return [
-            {
-              text: trasladoLocal.getAttribute('ImpLocTrasladado'),
-              fillColor: bgGrayColor,
-            },
-            {
-              text: [
-                {
-                  text: 'Tasa: ',
-                },
-                {
-                  text: `${(Number(trasladoLocal.getAttribute('TasadeTraslado')) * 1).toString()} %`,
-                },
-              ],
-              fillColor: bgGrayColor,
-            },
-            {
-              text: [
-                { text: 'Importe: ' },
-                { text: formatCurrency(trasladoLocal.getAttribute('Importe')) },
-              ],
-              alignment: 'right',
-              fillColor: bgGrayColor,
-            },
-          ];
-        }),
-      ],
-      dontBreakRows: true,
-    },
-    layout: 'tableLayout',
-  };
-
-  if (trasladosLocales.length === 0 && retencionesLocales.length === 0) {
-    totalesSubContent.push('', '');
-  }
-
-  if (trasladosLocales.length === 0 && retencionesLocales.length > 0) {
-    totalesSubContent.push(
-      {
-        ...retencionesLocalesTable,
-        colSpan: 2,
-      },
-      '',
-    );
-  }
-
-  if (trasladosLocales.length > 0 && retencionesLocales.length === 0) {
-    totalesSubContent.push(
-      {
-        ...trasladosLocalesTable,
-        colSpan: 2,
-      },
-      '',
-    );
-  }
-
-  if (trasladosLocales.length > 0 && retencionesLocales.length > 0) {
-    totalesSubContent.push(trasladosLocalesTable, retencionesLocalesTable);
-  }
-
-  totalesSubContent.push({
-    table: {
-      widths: ['*', '10%', 'auto'],
-      body: tableTotales,
-      dontBreakRows: true,
-    },
-    layout: 'tableLayout',
-  });
-
-  totalesContent.push(totalesSubContent);
-};
-
 const genericCfdiTotalesContent = (
   comprobante: XmlNodeInterface,
   catalogs: CatalogsData,
@@ -362,7 +200,7 @@ const genericCfdiTotalesContent = (
   if (impLocal10) {
     totalesSubContent.push('');
     totalesContent.push(totalesSubContent);
-    fillComplementoImpLocal10(impLocal10, primaryColor, bgGrayColor, tableTotales, totalesContent);
+    useImplocal10Complement(impLocal10, primaryColor, bgGrayColor, tableTotales, totalesContent);
   } else {
     totalesSubContent.push({
       table: {
