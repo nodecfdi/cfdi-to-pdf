6ced138b12d88fca17ec158ec74603ed6317124c
Fernando Isidro L <luffynando@gmail.com>
2025-01-26 20:31:48 -0600

    style: :rotating_light: resolve issues reported by latest eslint rule config

--- src/utils/break_characters.ts
+++ src/utils/break_characters.ts
@@ -1,5 +1,5 @@
 const breakCharacters = (raw: string, separator = '\u{200B}'): string => {
-  return [...raw].join(separator);
+  return [...new Intl.Segmenter().segment(raw)].map((segment) => segment.segment).join(separator);
 };
 
 export default breakCharacters;
